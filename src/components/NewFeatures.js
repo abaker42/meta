6d507e052b156fa0750b51fd4f2573ff0c561d74
abaker42 <anthony.baker390@gmail.com>
2023-09-17 20:10:32 -0400

    Whats new section done

--- src/components/NewFeatures.js
+++ src/components/NewFeatures.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styles from '@/styles'
+import Image from 'next/image'
 
-function NewFeatures() {
+function NewFeatures(props) {
+  const {imgUrl, title, subtitle} = props
   return (
-    <div>NewFeatures</div>
-  )
+		<div className='flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]'>
+			<div
+				className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+			>
+				<Image
+					src={imgUrl}
+					alt='icon'
+					height={70}
+					width={70}
+					className='w-1/2 h-1/2 object-contain'
+				/>
+			</div>
+			<h1 className='mt-[26px] font-bold text-[24px] leading-[30px] text-white'>
+				{title}
+			</h1>
+			<p className='flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]'>
+				{subtitle}
+			</p>
+		</div>
+	);
 }
 
 export default NewFeatures
--- src/sections/WhatsNew.js
+++ src/sections/WhatsNew.js
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'
+
+import { TitleText, TypingText } from "@/components/Text";
+import { newFeatures } from "@/constants";
+import styles from "@/styles";
+import { fadeIn, planetVariants, staggerContainer } from "@/utils/motion";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import getComet from "../../public/whats-new.png";
+import React from "react";
+import NewFeatures from "@/components/NewFeatures";
 
 function WhatsNew() {
+  
   return (
-    <div>WhatsNew</div>
-  )
+		<section className={`${styles.paddings} relative z-10`}>
+			<motion.div
+				variants={staggerContainer}
+				initial='hidden'
+				whileInView='show'
+				viewport={{ once: false, amount: 0.25 }}
+				className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+			>
+				<motion.div
+					variants={fadeIn("right", "tween", 0.2, 1)}
+					className='flex-[0.75] flex justify-center flex-col'
+				>
+					<TypingText title="| What's New?" />
+
+					<TitleText title={<>New findings in the Metaversos</>} />
+					<div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+						{newFeatures.map((feature) => (
+							<NewFeatures key={feature.title} {...feature} />
+						))}
+					</div>
+				</motion.div>
+				<motion.div
+					variants={planetVariants("right")}
+					className={`${styles.flexCenter} flex-1`}
+				>
+					<Image
+						src={getComet}
+						alt='whats new'
+						height={250}
+						width={250}
+						className='w-[90%] h-[90%] object-contain'
+					/>
+				</motion.div>
+			</motion.div>
+		</section>
+	);
 }
 
 export default WhatsNew
